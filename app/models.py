ab3010b1abd8d012f9cf0782098391e6dda36488
Vladyslav <your-email@example.com>
2025-10-20 21:04:04 +0300

    data of project

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    author = models.CharField(max_length=120)
+    year = models.IntegerField()
+
+
+    def __str__(self):
+        return self.title
